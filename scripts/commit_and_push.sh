a9e4cea983b5b6518410a33c64dd1012295535ab
Heinrich Weinz <weinzh@gmail.com>
2025-02-23 15:13:04 +0100

    Update Docker files and configurations

--- scripts/commit_and_push.sh
+++ scripts/commit_and_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-commit_and_push() {
-  git add .
-  git commit -m "update setup.sh and architecture.md"
-  git push origin main
-}
+#commit_and_push() {
+#  git add .
+#  git commit -m "update setup.sh and architecture.md"
+#  git push origin main
+#}
