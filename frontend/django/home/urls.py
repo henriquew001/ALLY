67b5ef1695cac226bd5a51d95ed50b100b442b20
Heinrich Weinz <weinzh@gmail.com>
2025-03-14 18:44:11 +0100

    Django projekt neu gestartet und aufgesetzt

--- frontend/django/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# app_name = 'home'  # This is the namespace
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path('<str:lang>/', views.home, name='home_lang'),
-]
--- frontend/django/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.utils.translation import activate
-
-def home(request, lang=None):
-    if lang:
-        activate(lang)
-    return render(request, 'home.html')
