0e3ff76fc5ed0951eb140dcbf8faafd4b81839a6
Heinrich Weinz <weinzh@gmail.com>
2025-03-15 19:42:57 +0100

    locale für app accounts erstellt

--- frontend/cofi/tests/home/test_header.py
+++ frontend/cofi/tests/home/test_header.py
@@ -1,49 +1,26 @@
-# /home/heinrich/projects/ConsciousFit/frontend/cofi/home/tests/test_header.py
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
-from django.conf import settings
-import os
+from django.utils.translation import gettext as _
+
 
 class HeaderTest(TestCase):
-    def test_logo_exists_and_has_correct_path(self):
-        """Testet, ob das Logo vorhanden ist und den korrekten Pfad hat."""
-        response = self.client.get('/en/')  # Korrigierter Namespace
-
-        self.assertEqual(response.status_code, 200)
-
-        logo_path = os.path.join(settings.STATIC_URL, 'img', 'cofi_logo_89x110.png')
-
-        # Überprüft, ob das Logo im HTML vorhanden ist und den korrekten Pfad hat
-        self.assertContains(response, f'<img src="{logo_path}" alt="Cofi Logo" class="logo">')
+    def setUp(self):
+        self.client = Client()
 
     def test_navigation_links_exist(self):
         """Testet, ob die Navigationslinks vorhanden sind."""
-        response = self.client.get('/en/')  # Korrigierter Namespace
+        response = self.client.get(reverse('home:home'))
 
         self.assertEqual(response.status_code, 200)
 
         # Überprüft, ob die Navigationslinks vorhanden sind
-        self.assertContains(response, '<a href="#">Home</a>')
+        self.assertContains(response, f'<a href="{reverse("home:home")}">Home</a>')
         self.assertContains(response, '<a href="#">About</a>')
         self.assertContains(response, '<a href="#">Services</a>')
         self.assertContains(response, '<a href="#">Contact</a>')
     
-    def test_brand_text_exists(self):
-        """Testet, ob der Brand Text vorhanden ist."""
-        response = self.client.get('/en/')  # Korrigierter Namespace
+    def test_login_link_exists_when_not_logged_in(self):
+        """Testet, ob der Login-Link angezeigt wird, wenn der Benutzer nicht angemeldet ist."""
+        response = self.client.get(reverse('home:home'))
         self.assertEqual(response.status_code, 200)
-
-        # Überprüft, ob die Text vorhanden ist
-        self.assertContains(response, '<span class="consciencia">Consciência</span>')
-        self.assertContains(response, '<span class="fitness">Fitness</span>')
-
-    def test_header_layout(self):
-      """Testet, ob das Layout im Header korrekt ist."""
-      response = self.client.get('/en/') # Korrigierter Namespace
-
-      self.assertEqual(response.status_code, 200)
-
-      # Überprüft, ob die Navigation unter dem Logo angezeigt wird
-      self.assertContains(response, '<header>')
-      self.assertContains(response, '<div class="logo-container">')
-      self.assertContains(response, '<nav>')
+        self.assertContains(response, f'<a href="{reverse("accounts:login")}">Login</a>')
