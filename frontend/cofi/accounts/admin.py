0effc29a62a8489159374fc7173f3d6e0cbeef40
Heinrich Weinz <weinzh@gmail.com>
2025-03-23 10:03:08 +0100

    Adminbereich: Benutzer ändern korrigiert

--- frontend/cofi/accounts/admin.py
+++ frontend/cofi/accounts/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'is_active', 'is_staff', 'is_superuser'] # Customize the list display in the admin
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('username', 'first_name', 'last_name')}), # Add a new fieldset for personal info
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser','groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}), # Add a new fieldset for important dates
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password2')}
+         ),
+    )
+    search_fields = ['email']
+    ordering = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- frontend/cofi/accounts/forms.py
+++ frontend/cofi/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(forms.ModelForm):
@@ -20,3 +21,9 @@ class CustomUserCreationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords do not match")
 
         return cleaned_data
+    
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'is_active', 'is_staff', 'is_superuser') # Include all the fields you want to edit in the admin.
+        # exclude = ('password',) # if you want to exclude password from the form
