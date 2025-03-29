f6fa39b1459c23b5a0ac66dd4e658ef51288e52d
Heinrich Weinz <weinzh@gmail.com>
2025-03-29 21:38:29 +0100

    settings geht auf profile seite

--- frontend/cofi/authentication/urls.py
+++ frontend/cofi/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from . import views
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('profile/', views.profile, name='profile'),
 ]
--- frontend/cofi/cofi/settings.py
+++ frontend/cofi/cofi/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'authentication',
     'focoquiz',
     'about',
-    'recipes',
-    'ingredients',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
