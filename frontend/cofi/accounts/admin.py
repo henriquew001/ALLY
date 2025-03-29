ba6fec6f0ee794b4e236019cfedee1b5a563b5b3
Heinrich Weinz <weinzh@gmail.com>
2025-03-29 19:31:29 +0100

    working, aber accounts fehlen in admin

--- frontend/cofi/accounts/admin.py
+++ frontend/cofi/accounts/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserAdmin(UserAdmin):
     # Customize the admin interface for CustomUser here
-    list_display = ('email', 'username', 'is_staff', 'is_active')
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('is_active',)}),
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'is_active')}),  # Modified: Added email, removed username, added is_active
+        (_('Personal Info'), {'fields': ('username', 'first_name', 'last_name')}), # Modified: Added username
+        (_('Permissions'), {
+            'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions'), # Modified: Removed is_active
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-
-admin.site.register(CustomUser, CustomUserAdmin)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password2')}
+         ),
+    )
+    search_fields = ['email']
+    ordering = ['email']
