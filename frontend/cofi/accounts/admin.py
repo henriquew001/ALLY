cc48022993b96e8a6c14f88ec7841db725627e92
Heinrich Weinz <weinzh@gmail.com>
2025-03-29 18:57:19 +0100

    est doicker in eigenem Unterordner

--- frontend/cofi/accounts/admin.py
+++ frontend/cofi/accounts/admin.py
@@ -1,13 +1,12 @@
-# /home/heinrich/projects/ConsciousFit/frontend/cofi/accounts/admin.py
-
 from django.contrib import admin
-from .models import UserPackage, CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-class UserPackageAdmin(admin.ModelAdmin):
-    list_display = ('user', 'package', 'start_date', 'end_date', 'is_active', 'canceled_at', 'next_payment_at')
-    list_filter = ('is_active',)
-    search_fields = ('user__email', 'package__name')
-    readonly_fields = ('start_date',)
+class CustomUserAdmin(UserAdmin):
+    # Customize the admin interface for CustomUser here
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_active',)}),
+    )
 
-admin.site.register(UserPackage, UserPackageAdmin)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
