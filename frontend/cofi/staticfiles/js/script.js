b70df55e652e62530984a458d3f7239773d59c55
Heinrich Weinz <weinzh@gmail.com>
2025-03-30 16:41:58 +0200

    ports in docker compose angepasst. dev auf interne sqlite umgestellt, system Testumgebung erstellt

--- frontend/cofi/staticfiles/js/script.js
+++ frontend/cofi/staticfiles/js/script.js
@@ -1,21 +1,25 @@
- // cofi/static/js/script.js
- document.addEventListener('DOMContentLoaded', function() {
-     const passwordToggles = document.querySelectorAll('.password-toggle');
+// cofi/static/js/script.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOMContentLoaded event fired');
+    const passwordToggles = document.querySelectorAll('.password-toggle');
+    console.log('passwordToggles:', passwordToggles);
 
-     passwordToggles.forEach(toggle => {
-         toggle.addEventListener('click', function() {
-             const passwordFieldId = this.id.replace('toggle-', '');
-             const passwordField = document.getElementById(passwordFieldId);
+    passwordToggles.forEach(toggle => {
+        toggle.addEventListener('click', function() {
+            console.log('Toggle clicked:', this);
+            // Find the password input field by traversing the DOM
+            const passwordField = this.closest('.password-input-container').querySelector('input');
+            console.log('passwordField:', passwordField);
 
-             if (passwordField.type === 'password') {
-                 passwordField.type = 'text';
-                 this.classList.remove('fa-eye');
-                 this.classList.add('fa-eye-slash');
-             } else {
-                 passwordField.type = 'password';
-                 this.classList.remove('fa-eye-slash');
-                 this.classList.add('fa-eye');
-             }
-         });
-     });
- });
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                this.classList.remove('fa-eye');
+                this.classList.add('fa-eye-slash');
+            } else {
+                passwordField.type = 'password';
+                this.classList.remove('fa-eye-slash');
+                this.classList.add('fa-eye');
+            }
+        });
+    });
+});
